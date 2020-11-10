c3d56c708329063a6e74ca4db3f22a3bd484b936
KurnakovMaksim <maksk210@gmail.com>
2020-11-10 18:11:04 +0900

    Added word and words entities.

---
+++ DictonaryXML.Domain/Entities/Word.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictonaryXML.Domain.Entities
+{
+    public class Word
+    {
+        public string MainWord { get; set; }
+        public string TranslationWord { get; set; }
+        public int PartOfSpeech { get; set; }
+        public int Gender { get; set; }
+        public string Description { get; set; }
+
+        public Word() { }
+
+        public Word(string mainWord,
+                    string translationWord,
+                    int partOfSpeech,
+                    int gender,
+                    string description)
+        {
+            MainWord = mainWord;
+            TranslationWord = translationWord;
+            PartOfSpeech = partOfSpeech;
+            Gender = gender;
+            Description = description;
+        }
+    }
+}
---
+++ DictonaryXML.Domain/Entities/Words.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictonaryXML.Domain.Entities
+{
+    public class Words
+    {
+        public List<Word> WordsList = new List<Word>();
+    }
+}
